0e352bf68c2ad627df46be15138db8a3d791a14c
pc <pc@F0X0W2J6PRGKFTO>
2017-10-10 13:59:01 +0800

    添加实体类属性

--- src/main/java/fire/common/entity/CompanyResult.java
+++ src/main/java/fire/common/entity/CompanyResult.java
@@ -8,11 +8,46 @@ public class CompanyResult extends Company{
 	private int CompanyId;
 	private int ManagerId;
 	private String NickName;
+	private String ManagerName;
+	private String Face;
+	private String Email;
+	private String Mobile;
+	private String Token;
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public int getCompanyId() {
+		return CompanyId;
+	}
+	public void setCompanyId(int companyId) {
+		CompanyId = companyId;
+	}
+	public int getManagerId() {
+		return ManagerId;
+	}
+	public void setManagerId(int managerId) {
+		ManagerId = managerId;
+	}
 	public String getNickName() {
 		return NickName;
 	}
 	public void setNickName(String nickName) {
 		NickName = nickName;
+	}
+	public String getManagerName() {
+		return ManagerName;
+	}
+	public void setManagerName(String managerName) {
+		ManagerName = managerName;
 	}
 	public String getFace() {
 		return Face;
@@ -32,40 +67,26 @@ public class CompanyResult extends Company{
 	public void setMobile(String mobile) {
 		Mobile = mobile;
 	}
-	private String Face;
-	private String Email;
-	private String Mobile;
-	public int getManagerId() {
-		return ManagerId;
-	}
-	public void setManagerId(int managerId) {
-		ManagerId = managerId;
-	}
-	public String getUserName() {
-		return UserName;
-	}
-	public void setUserName(String userName) {
-		UserName = userName;
-	}
-	public String getPassword() {
-		return Password;
-	}
-	public void setPassword(String password) {
-		Password = password;
-	}
-	public int getCompanyId() {
-		return CompanyId;
-	}
-	public void setCompanyId(int companyId) {
-		CompanyId = companyId;
-	}
-	private String Token;
 	public String getToken() {
 		return Token;
 	}
 	public void setToken(String token) {
 		Token = token;
 	}
+	@Override
+	public String toString() {
+		return "CompanyResult [UserName=" + UserName + ", Password=" + Password + ", CompanyId=" + CompanyId
+				+ ", ManagerId=" + ManagerId + ", NickName=" + NickName + ", ManagerName=" + ManagerName + ", Face="
+				+ Face + ", Email=" + Email + ", Mobile=" + Mobile + ", Token=" + Token + ", getId()=" + getId()
+				+ ", getName()=" + getName() + ", getCode()=" + getCode() + ", getLogo()=" + getLogo() + ", getIntro()="
+				+ getIntro() + ", getIndustry()=" + getIndustry() + ", getProvince()=" + getProvince() + ", getCity()="
+				+ getCity() + ", getArea()=" + getArea() + ", getAddress()=" + getAddress() + ", getPassed()="
+				+ getPassed() + ", getStatus()=" + getStatus() + ", getReorder()=" + getReorder() + ", getAddTime()="
+				+ getAddTime() + ", getRemark()=" + getRemark() + ", getLevelId()=" + getLevelId() + ", getTel()="
+				+ getTel() + ", getSN()=" + getSN() + ", getAtten()=" + getAtten() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + ", getClass()=" + getClass() + "]";
+	}
+	
 
 	
 }

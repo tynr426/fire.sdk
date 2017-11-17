71222c2dbe6ff69e891f989011899b8322ef8504
pc <pc@F0X0W2J6PRGKFTO>
2017-11-17 22:06:07 +0800

    生成jar包

---
+++ src/main/java/fire/common/entity/AssignmentResult.java
@@ -0,0 +1,19 @@
+package fire.common.entity;
+
+public class AssignmentResult extends Assignment{
+	private String Name;
+	private String Description;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	
+}
